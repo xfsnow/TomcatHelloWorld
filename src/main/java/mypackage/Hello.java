6a4056ab6a17ce45ac50441eec8fc934a5eda687
xfsnow <xuefeng@snowpeak.org>
2022-07-12 23:07:07 +0800

    compiler 版本再改回 1.8

--- src/main/java/mypackage/Hello.java
+++ src/main/java/mypackage/Hello.java
@@ -17,9 +17,9 @@ import jakarta.servlet.http.HttpServletResponse;
 public final class Hello extends HttpServlet {
 
    /**
-	 *
-	 */
-	private static final long serialVersionUID = 7590183391752570596L;
+    *
+    */
+   private static final long serialVersionUID = 7590183391752570596L;
 
    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
       Date date = new Date();
@@ -34,16 +34,17 @@ public final class Hello extends HttpServlet {
       PrintWriter writer = response.getWriter();
       StringBuilder sb = new StringBuilder();
       sb.append("<html><head>")
-      .append("<title>Sample Application Servlet</title>")
-      .append("</head>")
-      .append("<body bgcolor=\"white\">")
-      .append("<h1>Sample Application Servlet</h1>")
-      .append("<P>You're visiting from IP ").append(clientIp)
-      .append(". Current time is ").append(dateTimeStr)
-      .append("<p>This application is running on ").append(inetadd.getHostName()).append(" with IP ")
-      .append(inetadd.getHostAddress()).append(".</p>")
-      .append("<p>Please notice this time is updated after refreshing the page.</p>")
-      .append("<h3><a href=\"/hello\">Back to web app root.</a></h3><h3><a href=\"/\">Back to homepage.</a></h3></body></html>");
+            .append("<title>Sample Application Servlet</title>")
+            .append("</head>")
+            .append("<body bgcolor=\"white\">")
+            .append("<h1>Sample Application Servlet from Java Spring in Tomcat</h1>")
+            .append("<P>You're visiting from IP ").append(clientIp)
+            .append(". Current time is ").append(dateTimeStr)
+            .append("<p>This application is running on ").append(inetadd.getHostName()).append(" with IP ")
+            .append(inetadd.getHostAddress()).append(".</p>")
+            .append("<p>Please notice this time is updated after refreshing the page.</p>")
+            .append(
+                  "<h3><a href=\"/hello\">Back to web app root.</a></h3><h3><a href=\"/\">Back to homepage.</a></h3></body></html>");
       writer.println(sb);
    }
 }
